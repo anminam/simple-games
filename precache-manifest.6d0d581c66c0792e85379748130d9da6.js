e306a029d70adfcc0e0babca4a182fb63f6d1214
Anminam <minamhunmin@gmail.com>
2020-04-13 11:48:59 +0900

    Updates

---
+++ precache-manifest.6d0d581c66c0792e85379748130d9da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e38b79d180dea90ecf357281cb0d924",
+    "url": "/simple-games/index.html"
+  },
+  {
+    "revision": "341f5a46c0c67d62e3ea",
+    "url": "/simple-games/static/css/main.6ad97926.chunk.css"
+  },
+  {
+    "revision": "cb5b782d20dcc0242f79",
+    "url": "/simple-games/static/js/2.5e4f19be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-games/static/js/2.5e4f19be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341f5a46c0c67d62e3ea",
+    "url": "/simple-games/static/js/main.1b46f4ee.chunk.js"
+  },
+  {
+    "revision": "55bfde520be7eff58eee",
+    "url": "/simple-games/static/js/runtime-main.c70a05bb.js"
+  }
+]);
