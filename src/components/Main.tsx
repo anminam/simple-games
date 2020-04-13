64aad6e3c1c142a35f8200cfef1fc50069c1c783
Anminam <minamhunmin@gmail.com>
2020-04-13 16:00:31 +0900

    add router

---
+++ src/components/Main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Tictactoe from './Tictactoe';
+import Googoodan from './Googoodan';
+
+const Main = () => {
+
+    return (
+        <main className="App-main">
+          <Switch>
+            <Route path="/tictactoe" component={Tictactoe}></Route>
+            <Route path="/googoodan" component={Googoodan}></Route>
+            <Route expact path="/" component={Tictactoe}></Route>
+          </Switch>
+        </main>
+    )
+}
+
+export default Main;
