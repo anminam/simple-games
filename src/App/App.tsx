12b75c811e1e2ca85bde51fe43aa7fb180ea72da
Anminam <minamhunmin@gmail.com>
2020-04-14 10:15:01 +0900

    react router change to hashRouter

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Header from 'components/Header';
 import Main from 'components/Main';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <Main />
